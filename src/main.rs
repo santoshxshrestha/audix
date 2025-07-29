3e2b28b072881d7b172222c689784ea22e968e4d
santoshxshrestha <santoshxshrestha@gmail.com>
2025-07-29 17:51:08 +0545

    args

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,17 @@
+use clap;
+use clap::Arg;
+use clap::Command;
+fn cli() -> Command {
+    Command::new("audix")
+        .author("Santosh")
+        .about("command-line music player")
+        .arg(
+            Arg::new("music-dir")
+                .short('d')
+                .long("dir")
+                .value_name("DIRECTORY")
+                .required_unless_present("help"),
+        )
+        .arg(Arg::new("help").short("h").long("help"))
+}
 fn main() {}
